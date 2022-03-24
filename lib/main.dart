5db215550fec06826b15c6cd525929d09143b31a
Kvcss <kaiovsilva60@gmail.com>
2022-03-24 13:01:42 -0300

    Atualização de Bd

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 
-void main() {
+void main() async{
 
   //inicializar o Firebase
   WidgetsFlutterBinding.ensureInitialized();
-  Firestore.instance
+  await Firebase.initializeApp();
+  FirebaseFirestore db = FirebaseFirestore.instance;
+
+
+ db
       .collection("usuarios")
-      .document("pontuacao")
-      .setData({"Jamilton":"250"});
+      .doc("pontuacao")
+      .set({"Jamilton":"250", "Ana": "590"});
+
 
   runApp(MyApp());
 
