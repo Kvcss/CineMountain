be83ee433a0db61b3a88ecd7f4b58e18527b0e99
Kvcss <kaiovsilva60@gmail.com>
2022-03-25 02:43:11 -0300

    Tela Home Add

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:projetointegrado_e/Home.dart';
 
 
-void main() async{
+void main ()async{
 
   //inicializar o Firebase
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   FirebaseFirestore db = FirebaseFirestore.instance;
+//aloaloalo
 
-
- db
+  db
       .collection("usuarios")
       .doc("pontuacao")
       .set({"Jamilton":"250", "Ana": "590"});
+      //.doc()
 
 
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: Home(),debugShowCheckedModeBanner: false,
 
+  ));
 
-
-
-}
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
 }
