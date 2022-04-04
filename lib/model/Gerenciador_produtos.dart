a95f3804ea383311084f1707c5ddabe223a76b56
Kaio Vinicius <kaiovsilva60@gmail.com>
2022-04-04 17:48:21 -0300

    ajustes no adm

---
+++ lib/model/Gerenciador_produtos.dart
@@ -0,0 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class GerenciadorProduto {
+
+  GerenciadorProduto(){
+    _carregarTodosProdutos();
+  }
+
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  Future<void> _carregarTodosProdutos() async {
+    final QuerySnapshot snapProducts = await firestore.collection('produtos').get(); // Pega todos produtos da coleção
+
+    for(DocumentSnapshot doc in snapProducts.docs){
+      print(doc.data);
+    }
+  }
+}
