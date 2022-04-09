d8e8799ea1025b1724b29ab45f28760fafa83686
Kaio Vinicius <kaiovsilva60@gmail.com>
2022-04-09 19:13:08 -0300

    Tela de Cadastro de Filme- front 0/2

--- lib/admin/CadastroFilmeAdm.dart
+++ lib/admin/CadastroFilmeAdm.dart
@@ -12,60 +12,196 @@ class _CadastroFilmeState extends State<CadastroFilme> {
   Widget build(BuildContext context) {
     double _screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Stack(
-        children: <Widget>[
-          Column(
+      backgroundColor: Colors.white,
+      body: Container(
+        color: Colors.blue,
+        child: SingleChildScrollView(
+          child: Stack(
+            alignment: Alignment.topCenter,
             children: <Widget>[
-              SizedBox(
-                height: _screenHeight * .1, //strech
-              ),
-              Container(
-                color: Colors.black,
-                height: _screenHeight * .10,
-                child: Column(
-                  children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const <Widget>[
-                        Padding(
-                          padding: EdgeInsets.only(right: 0),
-                          child: Text(
-                            "Cadastro de",
-                            style: TextStyle(
-                                fontSize: 28,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold),
-                          ),
+              Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: _screenHeight * .1, //strech
+                  ),
+                  Container(
+                    color: Colors.black,
+                    height: _screenHeight * .10,
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const <Widget>[
+                            Padding(
+                              padding: EdgeInsets.only(right: 0),
+                              child: Text(
+                                "Cadastro de",
+                                style: TextStyle(
+                                    fontSize: 28,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const <Widget>[
-                        Text(
-                          "Filmes",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 28),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const <Widget>[
+                            Text(
+                              "Filmes",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 28),
+                            )
+                          ],
                         )
                       ],
-                    )
-                  ],
-                ),
-              )
+                    ),
+                  ),
+                  SizedBox(
+                    height: _screenHeight * .018,
+                  ),
+                  Positioned(
+                    top: 0,
+                    child: Container(
+                      color: Colors.red,
+                      height: _screenHeight *.07,
+                      child: Padding(
+                        padding: const EdgeInsets.all(7.0),
+                        child: TextField(
+                          //controller: _controllerSenha,
+                          keyboardType: TextInputType.text,
+                          style: const TextStyle(fontSize: 20),
+                          decoration: InputDecoration(
+                              contentPadding: const EdgeInsets.fromLTRB(3, 0, 0, 0),
+                              hintText: "Nome do Filme:",
+                              filled: true,
+                              fillColor: Colors.white,
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10))),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(0),
+                        child: Container(
+                          color: Colors.green,
+                          height: _screenHeight *.08,
+                          width: _screenHeight *.25,
+                          child: Padding(
+                            padding: const EdgeInsets.all(7.0),
+                            child: TextField(
+                              //controller: _controllerSenha,
+                              keyboardType: TextInputType.text,
+                              style: const TextStyle(fontSize: 15),
+                              decoration: InputDecoration(
+                                  contentPadding: const EdgeInsets.fromLTRB(5, 15, 15, 15),
+                                  hintText: "Data de Lançamento:",
+                                  filled: true,
+                                  fillColor: Colors.white,
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10))),
+                            ),
+                          ),
+                        ),
+                      ),
+                     const SizedBox(
+                        width: 65,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(0),
+                        child: Container(
+                          color: Colors.green,
+                          height: _screenHeight *.08,
+                          width: _screenHeight *.15,
+                          child: Padding(
+                            padding: const EdgeInsets.all(7.0),
+                            child: TextField(
+                              //controller: _controllerSenha,
+                              keyboardType: TextInputType.text,
+                              style: const TextStyle(fontSize: 15),
+                              decoration: InputDecoration(
+                                  contentPadding: const EdgeInsets.fromLTRB(5, 15, 15, 15),
+                                  hintText: "Duração:",
+                                  filled: true,
+                                  fillColor: Colors.white,
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10))),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                   SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(0),
+                        child: Container(
+                          color: Colors.green,
+                          height: _screenHeight *.08,
+                          width: _screenHeight *.25,
+                          child: Padding(
+                            padding: const EdgeInsets.all(7.0),
+                            child: TextField(
+                              //controller: _controllerSenha,
+                              keyboardType: TextInputType.text,
+                              style: const TextStyle(fontSize: 15),
+                              decoration: InputDecoration(
+                                  contentPadding: const EdgeInsets.fromLTRB(5, 15, 15, 15),
+                                  hintText: "Data de Lançamento:",
+                                  filled: true,
+                                  fillColor: Colors.white,
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10))),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 65,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(0),
+                        child: Container(
+                          color: Colors.green,
+                          height: _screenHeight *.08,
+                          width: _screenHeight *.15,
+                          child: Padding(
+                            padding: const EdgeInsets.all(7.0),
+                            child: TextField(
+                              //controller: _controllerSenha,
+                              keyboardType: TextInputType.text,
+                              style: const TextStyle(fontSize: 15),
+                              decoration: InputDecoration(
+                                  contentPadding: const EdgeInsets.fromLTRB(5, 15, 15, 15),
+                                  hintText: "Duração:",
+                                  filled: true,
+                                  fillColor: Colors.white,
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10))),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+
             ],
           ),
-          Positioned(
-            top: _screenHeight*.50,
-            left: 35,
-            child: Container(
-
-            ),
-          )
-
-        ],
+        ),
       ),
     );
   }
