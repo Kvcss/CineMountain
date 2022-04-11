50494f8ee916c8ce338978e21a4254160e82271a
Diego Bacci <diegobarbosa.sb@gmail.com>
2022-04-10 22:45:38 -0300

    Listagem produtos na Bomboniere

--- lib/usuario/Bomboniere.dart
+++ lib/usuario/Bomboniere.dart
@@ -1,5 +1,8 @@
-
 import 'package:flutter/material.dart';
+import 'package:projetointegrado_e/model/Gerenciador_produtos.dart';
+import 'package:projetointegrado_e/model/Produtos_ListTile.dart';
+//import 'package:model/Gerenciador_produtos.dart';
+
 class Bomboniere extends StatefulWidget {
   const Bomboniere({Key? key}) : super(key: key);
 
@@ -15,9 +18,16 @@ class _BomboniereState extends State<Bomboniere> {
         backgroundColor: Colors.black,
         title: const Text('Bomboniere', style: TextStyle(color: Colors.white),),
       ),
-      body: Image.network( ''
-
-      )
-    );
+      body: Consumer<GerenciadorProduto>(
+          builder: (_, gerenciadorProduto, __){
+            return ListView.builder(
+              itemCount: gerenciadorProduto.todosProdutos.lenght, // verificar pq nao esta roxo todosProdutos.lenght
+              itemBuilder: (_, index){
+                return ProdutoListTile(GerenciadorProduto.todosProdutos[index]); // todosProdutos nao esta roxo
+              }
+            );
+          },
+        ), // Aqui era pra ser consumer, declarar corretamente
+      ); // Item builder é para apresentar o
+    }
   }
-}
