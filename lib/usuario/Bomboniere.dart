dec0af0d2cfeba0ed3923732eac3d429d15c4294
Diego Bacci <diegobarbosa.sb@gmail.com>
2022-04-12 01:58:06 -0300

    Refatoração inicial Bomboniere.dart listagem fixa

--- lib/usuario/Bomboniere.dart
+++ lib/usuario/Bomboniere.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 //import 'package:model/Gerenciador_produtos.dart';
 
@@ -16,6 +18,34 @@ class _BomboniereState extends State<Bomboniere> {
         backgroundColor: Colors.black,
         title: const Text('Bomboniere', style: TextStyle(color: Colors.white),),
       ),
-      ); // Item builder é para apresentar o
-    }
+      backgroundColor: Colors.black,
+      body: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4)
+        ),
+        child: Container(
+          width: 150,
+          padding: const EdgeInsets.all(8),
+          child: Row(
+            children: <Widget>[
+              AspectRatio(aspectRatio: 1,
+                child: Image.network('https://www.google.com/imgres?imgurl=http%3A%2F%2Fpainel.menudigital.app.br%2Ffiles%2F59062020011853846115445.jpg&imgrefurl=https%3A%2F%2Fwww.cinepolis.me%2F&tbnid=V3G6_PmgGEMNbM&vet=12ahUKEwjUjbit1o33AhXdqJUCHew3AIIQMygHegUIARDNAQ..i&docid=cNgrsQfVDbZQCM&w=1000&h=1000&q=combo%20pipoca&ved=2ahUKEwjUjbit1o33AhXdqJUCHew3AIIQMygHegUIARDNAQ')
+              ),
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    /*
+                    Text(
+                      nome do item
+                    )
+                     */
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    ); // Item builder é para apresentar o
   }
+}
